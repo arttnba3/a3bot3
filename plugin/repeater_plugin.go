fa4f1f57ec44600a45925294306a3240f29d6e28
arttnba3 <arttnba3@outlook.com>
2022-05-21 14:41:44 -0700

    add RepeaterPlugin

---
+++ plugin/repeater_plugin.go
@@ -0,0 +1,35 @@
+package plugin
+
+import (
+	"a3bot3/api"
+	"a3bot3/event"
+)
+
+var lastMsgGroup = make(map[int64]string)
+var perGroupRepeated = make(map[int64]bool)
+
+type RepeaterPlugin struct {
+	PluginInfo
+}
+
+func (p *RepeaterPlugin) SendPrivateMsg(bot api.BotAPI, privateEvent event.PrivateEvent, messages []string) int {
+	// we only repeat in group
+	return MESSAGE_IGNORE
+}
+
+func (p *RepeaterPlugin) SendGroupMsg(bot api.BotAPI, groupEvent event.GroupEvent, messages []string) int {
+	groupId := groupEvent.GroupID
+
+	if lastMsgGroup[groupId] == groupEvent.Message {
+		if !perGroupRepeated[groupId] {
+			bot.SendGroupMsg(groupId, groupEvent.Message, false)
+			perGroupRepeated[groupId] = true
+		}
+	} else {
+		perGroupRepeated[groupId] = false
+	}
+
+	lastMsgGroup[groupId] = groupEvent.Message
+
+	return MESSAGE_BLOCK
+}
